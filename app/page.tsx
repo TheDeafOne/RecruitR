869164a9319a8bd90f6af4cd988102ce2b8647f2
TheDeafOne <Woodburnkb20@gcc.edu>
2024-03-01 19:53:50 -0500

    poc for resume downloads

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,41 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import getData from "./api/getData";
+import app from "@/firebase.config";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const handleForm = async () => {
-    getData({ collection_name: 'users' });
-    // const { result, error } = await addData('users', 'user-id', data)
+  app.name;
+  const fileRef = useRef<HTMLInputElement | null>(null);
+  const [file, setFile] = useState<File>();
+  const [url, setUrl] = useState<string>();
+  const handleChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+  const storage = getStorage();
 
-
-  }
+  useEffect(() => {
+    if (!file) return;
+    const storageRef = ref(storage, `resumes/${file.name}`);
+    uploadBytes(storageRef, file).then((snapshot): void => { // Added return type annotation
+      getDownloadURL(snapshot.ref).then((url) => {
+        setUrl(url);
+      });
+    });
+  }, [file]);
   return (
-    <div>
-      home
-      <Button onClick={handleForm}>
-        test
+    <>
+      <Button onClick={() => fileRef.current && fileRef.current.click()}>
+        <input id="upload" name="upload" type="file" ref={fileRef} hidden onChange={handleChange} />
+        upload resume
       </Button>
-    </div>
+      <div>
+        download files:
+        {url && <Link className="h-5 w-10" href={url} target="_blank">
+          {url}
+        </Link>}
+      </div>
+    </>
   )
 }
